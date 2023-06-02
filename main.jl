36cf9e2f46e7115456bcb5ffc7639ac66988b784
Dan Carpenter <dancarpenter21@gmail.com>
2023-06-02 09:52:00 -0400

    Learnt how to do funcshuns

---
+++ main.jl
@@ -0,0 +1,8 @@
+println("Matt K Chess - the chess bot build to defeat Matt K")
+
+# Dan is learning how to make functions
+function f(message)
+    println(message)
+end
+
+f("Boom checkmate Matt K")
